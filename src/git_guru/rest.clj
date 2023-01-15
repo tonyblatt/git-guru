e0479c8de86bc5a7acf82d5bb6ae5be441462690
Tony Blatt <tonyblatt@gmail.com>
2023-01-15 13:53:42 -0500

    Adding eastwood.

--- src/git_guru/rest.clj
+++ src/git_guru/rest.clj
@@ -1,13 +1,23 @@
 (ns git-guru.rest
-  (:import com.mashape.unirest.http.Unitrest))
+  (:require [clojure.string :as str])
+  ;(:import com.mashape.unirest.http.Unitrest)
+  )
 
 ; generic procedure for calling a rest function
 ; untested
-(defn perform-call [call-fn field-fn url-str param-seq]
+(defn perform-call
+  ""
+  [call-fn field-fn url-str param-seq]
   (let [params (split-at 2 param-seq)]
-    (reduce (fn [call-base [fst snd]] (. call-base field-fn fst snd)) (call-fn url-str) params)))
+    (reduce (fn [call-base [fst snd]] (. call-base field-fn fst snd)) (call-fn url-str) params))) 
 
 ; function for performing a POST call in rest
 ; untested
 (defn perform-post [addr & params]
-  (.. (Unirest/post addr) ))
+  ;(.. (Unirest/post addr) )
+  )
+
+(defn clojure-eastwood-test
+  ""
+  []
+  (clojure.string/replace "xyz" "x" "a"))
