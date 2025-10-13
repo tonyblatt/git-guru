c189620589fd9616750b8617ecefdbdcf66fe81d
tcb15 <tonyblatt@gmail.com>
2025-10-12 21:30:16 -0400

    Fixing the setup script.
    * Adding initial push script.
    * Adding initial finish and next script.
    * Adding complete branches script.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-if [ ! -f "$HOME/.bash_profile" ]; then
-    echo "bash profile not found. Creating it now..."
-    if touch "$HOME/.bash_profile"; then
-        echo "Successfully created bash profile'."
-    else
-        echo "Error: Failed to create bash profile." >&2
-        exit 1
-    fi
+if [[ "$SHELL" == *zsh ]]; then
+  profile_location="$HOME/.zshrc"
+elif [ -f "$HOME/.bash_profile" ]; then
+  profile_location="$HOME/.bash_profile"
+elif [ -f "$HOME/.bashrc" ]; then
+  profile_location="$HOME/.bashrc"
+elif [ -f "$HOME/.bash_login" ]; then
+  profile_location="$HOME/.bash_login"
 else
-    echo "File bash profile already exists. Skipping creation."
+  profile_location="$HOME/.profile"
 fi
 
 setup_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 script_dir="${setup_dir%?????}scripts"
 
 add_scripts_to_path="export PATH=\"\$PATH:$script_dir\""
-if grep -Fxq "$add_scripts_to_path" "$HOME/.bash_profile"; then
+if grep -Fxq "$add_scripts_to_path" "$profile_location"; then
 #if grep -Fxq "$add_scripts_to_path" "$script_dir/gui"; then
     echo "Skipping adding script dir to the bash profile because it was already present."
 else
-    printf "\n%s\n" "$add_scripts_to_path" >> "$HOME/.bash_profile"
+    printf "\n%s\n" "$add_scripts_to_path" >> "$profile_location"
     echo "Added script dir to bash profile."
 fi
