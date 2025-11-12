de78cf4e1be95ff64e43eb55afb19238f55b992d
wimsing <tonyblatt@gmail.com>
2025-11-11 22:02:29 -0500

    Adding param completion for the checkout completion script.

--- scripts/checkout-completion.bash
+++ scripts/checkout-completion.bash
@@ -4,8 +4,11 @@ _tab_complete()
   prev_index=$((COMP_CWORD - 1))
   prev_word=${COMP_WORDS[prev_index]}
   local current_word=${COMP_WORDS[COMP_CWORD]}
+  first_letter=${current_word:0:1}
   if [ "$prev_word" = "-t" ] || [ "$prev_word" = "--tag" ]; then
     complete_string=$(git tag | tr '\n' ' ')
+  elif [ "-" = "$first_letter" ]; then
+      complete_string="-t --tag -p --primary-branch -h --help"
   else
     branch_list=$(git branch)
     complete_string=$(echo "$branch_list" | tr '\n' ' ' | tr -d '*' | tr -s ' ' | sed 's/^ *//')
