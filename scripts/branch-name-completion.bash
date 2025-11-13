b13eabbc6869182af5f48165c4ff53d62fe46aba
wimsing <tonyblatt@gmail.com>
2025-11-12 22:08:22 -0500

    Cleanup.

--- scripts/branch-name-completion.bash
+++ scripts/branch-name-completion.bash
@@ -3,8 +3,8 @@ _branch_name_complete()
 {
   branch_list=$(git branch)
   branch_string=$(echo "$branch_list" | tr '\n' ' ' | tr -d '*' | tr -s ' ' | sed 's/^ *//')
-    local current_word=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=($(compgen -W "$branch_string" -- "$current_word"))
+  local current_word=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=($(compgen -W "$branch_string" -- "$current_word"))
 }
 
 complete -F _branch_name_complete "complete_branches"
